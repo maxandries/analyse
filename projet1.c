d955582af53fee7fbe506ae77e0eb85708ab4251
maxandries <maxime.andries@student.uclouvain.be>
2016-03-06 16:51:34 +0100

    test

---
+++ projet1.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+struct block_header {
+	unsigned int size : 29,
+		     zero: 2,
+		     alloc: 1;
+};
+
+struct block{
+	struct block_header header;
+	struct block *next;
+};
+
+void* malloc(size_t size){
+	struct block_header *a;
+	struct block *b;
+	a->size = size;
+	a->alloc = 1;
+	
+}
+
+int main(int argc, int *argv[]){
+printf("%d\n", sizeof(struct block_header));
+}
