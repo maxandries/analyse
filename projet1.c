ed5189024b008bc895157a115ff7dd861cbeb99f
maxandries <maxime.andries@student.uclouvain.be>
2016-03-08 22:05:58 +0100

    Update projet1.c

--- projet1.c
+++ projet1.c
@@ -11,12 +11,12 @@ struct block_header {
 };
 
 struct block_header *base;
-base->size = sbrk(0)-(base+4);
+base->size = (void *)sbrk(0)-(base+4);
 base->alloc = 0;
 
 
 struct block_meta *find_free_block(size_t size) {
-  struct block_header *current = global_base;
+  struct block_header *current = base;
   while (current && !(current->alloc && current->size >= size)) {
     current = (current + 4 + current->size);
   }
