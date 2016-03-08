e94694209db6b67f5ade274766d9e881ac495306
maxandries <maxime.andries@student.uclouvain.be>
2016-03-08 16:07:37 +0100

    Update projet1.c

--- projet1.c
+++ projet1.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define SIZE_HEADER sizeof(struct block_header)
+
 struct block_header {
 	unsigned int size : 29,
 		     zero: 2,
 		     alloc: 1;
 };
 
+void* findFree(size_t size){
+	
+}
+
 void* malloc(size_t size){
-	struct block_header *a;
-	struct block *b;
-	a->size = size;
-	a->alloc = 1;
 	
 }
 
