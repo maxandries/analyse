9762e96d29358143f814359234707481a8c8668a
maxandries <maxime.andries@student.uclouvain.be>
2016-03-10 17:32:50 +0100

    modification first et mise en commentaire de find_Block

--- projet1.c
+++ projet1.c
@@ -5,6 +5,7 @@
 #define SIZE_HEADER sizeof(struct block_header)
 int taille_heap = 4000000;
 void *base = NULL;
+struct block_header *first;
 struct block_header {
 	unsigned int size : 29,
 		     zero: 2,
@@ -12,10 +13,15 @@ struct block_header {
 };
 
 
-struct block_meta *find_free_block(size_t size) {
-
+/*struct block_head *find_free_block(struct block_meta **last, size_t size) {
+  struct block_head *current = *first;
+  while (current && !(current->free && current->size >= size)) {
+    *last = current;
+    current = current->next;
+  }
+  return current;
 }
-
+*/
 struct block_header* splitBlock(struct block_header name){
 	
 }
@@ -33,8 +39,8 @@ void* malloc(size_t size){
 	
 	if(!(base)){
 		base = sbrk(0);
-		sbrk(taille_heap+4);
-		struct block_header *first;
+		sbrk(taille_heap);
+		*first = *base;
 		first->size = taille_heap;
 		first->alloc =0;
 	}
