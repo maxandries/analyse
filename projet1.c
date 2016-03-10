e4e4d9f4e3314cf56b7c8a1cfb177a7f82ac300d
maxandries <maxime.andries@student.uclouvain.be>
2016-03-10 16:49:02 +0100

    ajout taille heap

--- projet1.c
+++ projet1.c
@@ -3,25 +3,16 @@
 #include <unistd.h>
 
 #define SIZE_HEADER sizeof(struct block_header)
-
+size_t taille_heap = 4000000;
 struct block_header {
 	unsigned int size : 29,
 		     zero: 2,
 		     alloc: 1;
 };
 
-struct block_header *base;
-
-*base->size = (void *)sbrk(0)-(base+4);
-*base->alloc = 0;
-
 
 struct block_meta *find_free_block(size_t size) {
-  struct block_header *current = base;
-  while (current && !(current->alloc && current->size >= size)) {
-    current = (current + 4 + current->size);
-  }
-  return *current;
+
 }
 
 struct block_header* splitBlock(struct block_header name){
