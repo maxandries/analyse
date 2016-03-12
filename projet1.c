296b3575040d1959df9014d23aad38bdf64e5360
maxandries <maxime.andries@student.uclouvain.be>
2016-03-12 23:17:08 +0100

    Update projet1.c

--- projet1.c
+++ projet1.c
@@ -13,13 +13,8 @@ struct block_header *first = NULL;
 int memsize = 4000000;
 
 struct block_header *findFree(size_t size) {
-	printf("test find\n");
 	struct block_header *current = first;
-	printf("current OK \n");
-	int i =1;
 	while (!(current->alloc == 0 && (current->size == size || (current->size)-size > 4))) {
-		printf("test : %d\n", i);
-		i++;
 		if ((current+current->size) == sbrk(0)){
 			return NULL;
 		}
@@ -30,35 +25,28 @@ struct block_header *findFree(size_t size) {
 
 
 void* mymalloc(size_t size){
-	printf("test\n");//debug
 	if(size <= 0){
-		printf("null");//debug
 		return NULL;
 		
 	}
 	
 	int sizeTot = align4(size) + SIZE_HEADER; //alignement sur 32bits et ajouts de la taille de la structure
-	printf("sizeTot : %d\n", sizeTot);//debug
 	if(!first){
-		printf("test 2\n");//debug
 		first = sbrk(0);
 		sbrk(memsize);
 		first->size = memsize;
 		first->alloc = 0;
-		printf("%d \n", (sbrk(0)-(void *)first));
 	}
-	printf("not null at first call?\n");//debug
 	struct block_header *newBlock = findFree(sizeTot);
 	if(!newBlock){
-		printf("findfree null\n");
 		return NULL; //pas de place libre
 	}
-	printf("findfree ok\n");
 	if(newBlock->size == sizeTot){
 		
 		return (void *)(newBlock+1);
 	}
 	if(sizeTot<newBlock->size){
+		printf("test\n")
 		int sizep = (newBlock->size)-sizeTot;
 		newBlock->size = sizeTot;
 		struct block_header *freeOne = (newBlock->size)/4+newBlock;
