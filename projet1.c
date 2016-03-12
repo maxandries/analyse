58cdd8e13d1c58847fba91d5f628650aebff640f
maxandries <maxime.andries@student.uclouvain.be>
2016-03-12 17:20:28 +0100

    Update projet1.c

--- projet1.c
+++ projet1.c
@@ -66,6 +66,7 @@ void* mymalloc(size_t size){
 		struct block_header *newBlock = freeOne+(freeOne->size);
 		printf("struct created: %p \n", newBlock);
 		printf("size: %d\n", sizeTot);
+		printf("difference :%p", sbrk(0)-newBlock);
 		newBlock->size = sizeTot;
 		printf("size allocated\n");
 		newBlock->alloc = 1;
