124fc39f421dcce5722a34eed0c6ef0e224902cc
maxandries <maxime.andries@student.uclouvain.be>
2016-03-12 16:13:55 +0100

    Update projet1.c

--- projet1.c
+++ projet1.c
@@ -13,8 +13,9 @@ struct block_header *first = NULL;
 int memsize = 40000;
 
 struct block_header *findFree(size_t size) {
-  struct block_header *current = *first;
-  printf("current OK\n");
+	printf("test find");
+  struct block_header *current = first;
+  printf("current OK \n");
   while (!(current->alloc && (current->size == size || current->size - size > 4))) {
   	if ((current+current->size) == sbrk(0)){
   		return NULL;
