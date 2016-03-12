23583f3261544a01d2644daf10edfdcdc9b059ad
maxandries <maxime.andries@student.uclouvain.be>
2016-03-12 17:25:35 +0100

    Update projet1.c

--- projet1.c
+++ projet1.c
@@ -10,7 +10,7 @@ struct block_header {
 		     alloc: 1;
 };
 struct block_header *first = NULL;
-int memsize = 40000;
+int memsize = 4000000;
 
 struct block_header *findFree(size_t size) {
 	printf("test find\n");
