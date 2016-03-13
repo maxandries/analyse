02e7cd5fdb38246a0c1b33f6c9f133dda4889300
maxandries <maxime.andries@student.uclouvain.be>
2016-03-13 15:32:45 +0100

    Update projet1.c

--- projet1.c
+++ projet1.c
@@ -102,5 +102,6 @@ int *test = (int *)mymalloc(sizeof(int));
 printf("adresse int3 : %p\n",test);
 int *test1 = (int *)mycalloc(sizeof(int));
 printf("adresse int4 : %p\n",test1);
+printf("diff int3 et int4 : %d", test1-test);
 
 }
