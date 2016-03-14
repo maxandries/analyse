de511ae575d6fd79294de7ea665da9720a45e378
maxandries <maxime.andries@student.uclouvain.be>
2016-03-14 19:13:33 +0100

    Create mymalloc.h

---
+++ mymalloc.h
@@ -0,0 +1,14 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+struct block_header {
+	unsigned int size : 29,
+		     zero: 2,
+		     alloc: 1;
+};
+
+struct block_header *findFree(size_t size);
+void* mymalloc(size_t size);
+void *mycalloc(size_t size);
+void myfree(void *ptr);
+ 
+#endi
