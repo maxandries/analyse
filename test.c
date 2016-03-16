73fd40bd30b1e85a1667a21acc8878feba510827
maxandries <maxime.andries@student.uclouvain.be>
2016-03-16 16:40:28 +0100

    Update test.c

--- test.c
+++ test.c
@@ -13,17 +13,23 @@ long timeval_diff(struct timeval *t2, struct timeval *t1) {
 int main(){
 
 
-  struct timeval tvStart1, tvEnd1, tvStart2, tvEnd2;
-  int i;
+  struct timeval tvStart1, tvEnd1, tvStart2, tvEnd2, tvStart3, tvEnd3, tvStart4, tvEnd4 ;
   gettimeofday(&tvStart1, NULL);
   int *mymall = (int *)mymalloc(100*sizeof(int));
   long *mymall2 = (long *)mymalloc(10*sizeof(long));
   gettimeofday(&tvEnd1, NULL);
-  printf("adresse 1: %p\n adresse 2 : %p\n",mymall, mymall2);
   gettimeofday(&tvStart2, NULL);
   int *mall = (int *)malloc(100*sizeof(int));
   long *mall2 = (long *)malloc(10*sizeof(long));
   gettimeofday(&tvEnd2, NULL);
-  printf("adresse 1: %p\n adresse 2 : %p\n",mall, mall2);
   printf("Duration mymalloc : %ld microseconds\nDuration malloc : %ld microseconds\n", timeval_diff(&tvEnd1, &tvStart1), timeval_diff(&tvEnd2, &tvStart2));
+  gettimeofday(&tvStart3, NULL);
+  int *mycall = (int *)mycalloc(100*sizeof(int));
+  long *mycall2 = (long *)mycalloc(10*sizeof(long));
+  gettimeofday(&tvEnd3, NULL);
+  gettimeofday(&tvStart4, NULL);
+  int *call = (int *)calloc(100*sizeof(int));
+  long *call2 = (long *)calloc(10*sizeof(long));
+  gettimeofday(&tvEnd4, NULL);
+  printf("Duration mycalloc : %ld microseconds\nDuration calloc : %ld microseconds\n", timeval_diff(&tvEnd3, &tvStart3), timeval_diff(&tvEnd4, &tvStart4));
 }
