9f02bd9fbb3d615be869e1a590048a3e638bb6fb
maxandries <maxime.andries@student.uclouvain.be>
2016-03-16 13:33:09 +0100

    Update test.c

--- test.c
+++ test.c
@@ -16,10 +16,10 @@ int main(){
   struct timeval tvStart1, tvEnd1, tvStart2, tvEnd2;
   int i;
   gettimeofday(&tvStart1, NULL);
-  int *mymall = (int *)mymalloc(sizeof(int));
+  int *mymall = (int *)mymalloc(100*sizeof(int));
   gettimeofday(&tvEnd1, NULL);
   gettimeofday(&tvStart2, NULL);
-  int *mall = (int *)malloc(sizeof(int));
+  int *mall = (int *)malloc(100*sizeof(int));
   gettimeofday(&tvEnd2, NULL);
   
   printf("Duration mymalloc : %ld microseconds\nDuration malloc : %ld\n", timeval_diff(&tvEnd1, &tvStart1), timeval_diff(&tvEnd2, &tvStart2));
