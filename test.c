1c07329d7834421029f7b998d54ad22ead95705e
maxandries <maxime.andries@student.uclouvain.be>
2016-03-16 13:10:41 +0100

    Update test.c

--- test.c
+++ test.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include "mymalloc.h"
 #include <sys/time.h>
+
+
+long timeval_diff(struct timeval *t2, struct timeval *t1) { 
+  long diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec); 
+  return (diff);
+} 
+
+
 int main(){
   
 }
